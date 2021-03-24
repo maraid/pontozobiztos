b0977dbbbd5028c51769f9d93ce98c6549c37e5d
Daniel Marai <danielmarai@yahoo.com>
2021-03-24 22:28:58 +0100

    Link preview now searches the whole string, not just the beginning.

--- pontozobiztos/plugins/link_preview/__init__.py
+++ pontozobiztos/plugins/link_preview/__init__.py
@@ -20,38 +20,37 @@ def upload_with_retries(client, image):
 
 
 def on_message(thread: fbchat.Group, author, message):
-    if not message.text.startswith('https://'):
-        return False
+    if match := re.search(r'(https://[^\s]*)', message.text):
+        url = match.group(1)
+        client = fbchat.Client(session=thread.session)
+        try:
+            title, description, image_url = web_preview(
+                url, parser='html.parser', timeout=2000,
+                headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36'})
+        except requests.exceptions.InvalidURL:
+            return False
+        except requests.exceptions.Timeout:
+            return True
 
-    client = fbchat.Client(session=thread.session)
-    try:
-        title, description, image_url = web_preview(
-            message.text, parser='html.parser', timeout=2000,
-            headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36'})
-    except requests.exceptions.InvalidURL:
-        return False
-    except requests.exceptions.Timeout:
+        logger.info(f'Fetched preview. Title: "{title}".'
+                    f' Description: "{description}"'
+                    f' Image URL: "{image_url}"')
+
+        if not (title and image_url):
+            logger.debug(f'Incorrect preview data.')
+            return False
+
+        if image_url.startswith('/'):
+            if match := re.search(r'^(https://[^/]+).*', url):
+                image_url = match.group(1) + image_url
+        r = requests.get(image_url)
+
+        files = upload_with_retries(client, r.content)
+        title = '*' + title + '*\n' if title else ''
+        # description = description + '\n' if description else ''
+
+        # thread.send_text(text=f'{title}{description}',
+        thread.send_text(text=f'{title}',
+                         files=files,
+                         reply_to_id=message.id)
         return True
-
-    logger.info(f'Fetched preview. Title: "{title}".'
-                f' Description: "{description}"'
-                f' Image URL: "{image_url}"')
-
-    if not (title and image_url):
-        logger.debug(f'Incorrect preview data.')
-        return False
-
-    if image_url.startswith('/'):
-        if match := re.search(r'^(https://[^/]+).*', message.text):
-            image_url = match.group(1) + image_url
-    r = requests.get(image_url)
-
-    files = upload_with_retries(client, r.content)
-    title = '*' + title + '*\n' if title else ''
-    # description = description + '\n' if description else ''
-
-    # thread.send_text(text=f'{title}{description}',
-    thread.send_text(text=f'{title}',
-                     files=files,
-                     reply_to_id=message.id)
-    return True
