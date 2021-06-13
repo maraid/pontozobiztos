8e0438e10dd871a569aa83308b24fbb01fdd63b3
Daniel Marai <danielmarai@yahoo.com>
2021-06-13 20:54:13 +0200

    Add error logging on fbchat listen()

--- main.py
+++ main.py
@@ -10,4 +10,8 @@ from pontozobiztos.HomoBot import HomoBot
 if __name__ == "__main__":
     logger.info('PONTOZOBOT starting up!')
     bot = HomoBot.create()
-    bot.listen()
+    try:
+        bot.listen()
+    except Exception as e:
+        logger.error(str(e))
+        raise
