58a8a9b2ea1f5f2a7c5ebdb811fbcefac7efd3d7
maraid <danielmarai@yahoo.com>
2021-09-27 08:20:12 +0200

    Added hungarian double and triple letters to ping

--- pontozobiztos/plugins/ping/__init__.py
+++ pontozobiztos/plugins/ping/__init__.py
@@ -9,3 +9,7 @@ def on_message(message, author):
     
     if formatted[1:] == 'ing':
         message.thread.send_text(formatted[0] + 'ong')
+    elif formatted[2:] == 'ing' and formatted[:2] in ('cs', 'dz', 'gy', 'ly', 'ny', 'sz', 'ty', 'zs'):
+        message.thread.send_text(formatted[:2] + 'ong')
+    elif formatted == 'dzsing':
+        message.thread.send_text('dzsong')
